19e6fc9a16a562aa5a9de0cb1a731e7b7e6bf64d
Kandruk Konstantin <kandruk@ya.ru>
2022-03-04 00:06:02 +0700

    dao-fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,6 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
   const DAO = await ethers.getContractFactory("DAO");
+  // без реального деплоя
   const greeter = await DAO.deploy();
 
   await greeter.deployed();
